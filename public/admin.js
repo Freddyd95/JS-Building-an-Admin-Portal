e205400a5b46457d6f8d7f94d8085975c8d89893
Freddyd95 <freddydiaz28@hotmail.com>
2024-07-09 17:45:34 -0700

    updated admin portal

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
 
-// Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    let div = document.querySelector('#root');
+    div.computedStyleMap.listStyleType = 'none';
+
+    books.forEach(bookQuantity)
+}
+
+function bookQuantity(book) {
+    let div = document.querySelector('#root')
+    let li = document.createElement('li')
+    li.textContent = book.title
+    let availableInput = document.createElement('input')
+    availableInput.value = book.quantity
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: availableInput.value
+            })
+        })
+    })
+    li.append(availableInput, saveButton)
+    div.append(li)
+}
+main();
